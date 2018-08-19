de4e3685e7f48d18db2b49d380f28c1b6173796a
jps-e <jps-e@users.noreply.github.com>
2018-08-19 10:16:55 -0700

    Highlight entity occurrences in kindleunpack rawml.
    occ_bold.pl -raw book.rawml < entity.occ > book.html
    
    entity.occ is the output of entity_occ.pl
    
    Options:
    -raw rawfile
    -out outfile (default STDOUT)
    -tag "label string"  (search aid)
    -num (precede highligted string with the occ # in [])
    
    The purpose of the occurrence # in [] is to aid knowing
    which occurrence is being highligted.
    
    A good label string is the entity name, which will be
    enclosed in "[]" to help ensure uniqueness.

---
+++ Scripts/occ_bold.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($beg, $siz, $n, $num, $out, $raw, $tag, $txt, $fh_out);
+my $b = '<b>'; my $eb = '</b>';
+my $pos = -14; my $i = 0;
+GetOptions(
+           "num"   => \$num,
+           "out=s" => \$out,
+           "raw=s" => \$raw,
+           "tag=s" => \$tag
+          );
+die "No raw ML input file specified." unless $raw;
+open FHraw, $raw or die "Can't open $raw for reading";
+if ($out) { open $fh_out, ">", $out; *STDOUT = $fh_out; }
+while(<>) {
+  ($beg, $siz) = split;
+  $n = read FHraw, $txt, $beg - $pos;
+  print $txt;
+  if ($tag) { print "[$tag] "; }
+  if ($num) { printf "[%d] ", $num++; }
+  $pos += $n;
+  $n = read FHraw, $txt, $siz;
+  print "$b$txt$eb";
+  $pos += $n;
+}
