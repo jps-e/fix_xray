7981def6c5b6927f718835fefae47f9999f47a87
jps-e <jps-e@users.noreply.github.com>
2018-08-25 09:33:16 -0700

    Add occ_list.pl
    which prints extracts from a rawml file based on
    offsets and lengths read from stdin derived from
    the occurrence table in an XRAY file.

---
+++ Scripts/occ_list.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($beg, $siz, $pad, $raw, $txt);
+GetOptions(
+           "pad=i" => \$pad,
+           "raw=s" => \$raw
+          );
+die "No raw ML input file specified." unless $raw;
+open FH, $raw;
+while(<>) {
+  ($beg, $siz) = split;
+  $beg += 14;
+  if ($pad) { $beg -= $pad; $siz += 2*$pad; }
+  seek FH, $beg, 0;
+  my $n = read FH, $txt, $siz;
+  print "$txt\n";
+}
