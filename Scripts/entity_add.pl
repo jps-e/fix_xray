4ffc4b211cf9f6a24ccf6a4e48c117069c04db2b
jps-e <jps-e@users.noreply.github.com>
2018-09-02 15:17:08 -0700

    Add prototyp scripts to add and split entities.

---
+++ Scripts/entity_add.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($count, $id, $name, $source, $term, $text, $type, $wildcard);
+my $fmt = 'INSERT INTO "entity" VALUES(%d, %s, NULL, %d, %d, 1);\n';
+my $fmt2 = 'INSERT INTO "entity_description" VALUES(%s, %s, %d, %d);\n';
+my $fmt3 = 'INSERT INTO "entity_description" VALUES(%s, %s, NULL, %d);\n';
+GetOptions(
+           "count=i"  => \$count,
+           "id=i"     => \$id,
+           "name=s"   => \$name,
+           "source=i" => \$source,
+           "term"     => \$term,
+           "description=s" => \$text,
+           "wildcard=s" => \$wildcard
+          );
+die "must supply count" unless $count;
+die "must supply id" unless $id;
+if ($term) { $type = 2; } else { $type = 1; }
+printf $fmt, $id, $name, $type, $count;
+if ($source) {
+  printf $fmt2, $text, $wildcard, $source, $id;
+} else {
+  printf $fmt3, $text, $wildcard, $id;
+}
---
+++ Scripts/entity_split.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($entity, $beg, $end, $name, $who);
+my $fmt = 'UPDATE "occurrence" SET entity=%d where start=%d AND length =%d\n';
+GetOptions(
+           "entity=i" => \$entity,
+           "who=s" => \$who
+          );
+die "must supply entity" unless $entity;
+die "must supply who" unless $who;
+while(<>) {
+  ($beg, $end, $name) = split /\t|$/;
+  printf $fmt, $entity, $beg, $end if $name =~ /$who/;
+}
