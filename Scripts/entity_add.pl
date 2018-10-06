a4e169d4d1c99136b2432cbb1f45863415603754
jps-e <jps-e@users.noreply.github.com>
2018-10-06 15:29:17 -0700

    Change variable $fmt to $fmt1

--- Scripts/entity_add.pl
+++ Scripts/entity_add.pl
@@ -4,7 +4,7 @@ use warnings;
 use Getopt::Long;
 
 my ($count, $id, $name, $source, $term, $text, $type, $wildcard);
-my $fmt = "INSERT INTO \"entity\" VALUES(%d, %s, NULL, %d, %d, 1);\n";
+my $fmt1 = "INSERT INTO \"entity\" VALUES(%d, %s, NULL, %d, %d, 1);\n";
 my $fmt2 = "INSERT INTO \"entity_description\" VALUES(%s, %s, %d, %d);\n";
 my $fmt3 = "INSERT INTO \"entity_description\" VALUES(%s, %s, NULL, %d);\n";
 GetOptions(
@@ -19,7 +19,7 @@ GetOptions(
 die "must supply count" unless $count;
 die "must supply id" unless $id;
 if ($term) { $type = 2; } else { $type = 1; }
-printf $fmt, $id, $name, $type, $count;
+printf $fmt1, $id, $name, $type, $count;
 if ($source) {
   printf $fmt2, $text, $wildcard, $source, $id;
 } else {
