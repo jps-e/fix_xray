1634fe75b69c5c6257024a314acdf791a2734d4e
jps-e <jps-e@users.noreply.github.com>
2018-11-22 09:28:59 -0800

    Add option to filter by entity.

--- Scripts/excerpt_bold.pl
+++ Scripts/excerpt_bold.pl
@@ -3,10 +3,11 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-my ($n, $num, $out, $raw, $tag, $txt, $fh_out, $xray);
+my ($ent, $n, $num, $out, $raw, $tag, $txt, $fh_out, $xray);
 my $b = '<b>'; my $eb = '</b>';
 my $pos = -14;
 GetOptions(
+           "entity=i" => \$ent,
            "num"   => \$num,
            "out=s" => \$out,
            "raw=s" => \$raw,
@@ -22,6 +23,10 @@ my @result = `echo "$sel" | sqlite3 $xray`;
 for (@result) {
   my ($id, $beg, $siz, $image, $related, $go) = split /\|/;
   last if $image;
+  if ($ent) {
+    $num = $id if $num and $related =~ /^$ent,|,$ent,|,$ent$/;
+    next unless $related =~ /^$ent,|,$ent,|,$ent$/;
+  }
   $n = read FHraw, $txt, $beg - $pos;
   print $txt;
   if ($tag) { print "[$tag] "; }
