69123fb8548ae4b4ad3811b487bf7c72eb848253
jps-e <jps-e@users.noreply.github.com>
2018-12-23 15:25:28 -0800

    Initial string_locs.pl to get sring locations.
    Print start and length of string occurrances in a text file.

---
+++ string_locs.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($inp, $str, $l, $txt);
+my $offset = -14;
+GetOptions(
+           "inp=s"  => \$inp,
+           "offset" => \$offset,
+           "str=s"  => \$str,
+           "l=i"    => \$l
+           );
+$txt = `cat $inp`;
+my $loc = 0;
+while (($loc = index($txt, $str, $loc + 1)) > 0) {
+  printf "%d\t10\n", $loc + $offset;
+}
