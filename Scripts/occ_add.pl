00a8fc518f7948640bfc6ecddaad74230a9b2b19
jps-e <jps-e@users.noreply.github.com>
2018-12-28 08:36:36 -0800

    occ_add.pl adds occurrences not already in Xray.

---
+++ Scripts/occ_add.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($beg, $siz, $entity, $out, $fh_out);
+GetOptions(
+           "entity=i" => \$entity,
+           "out=s" => \$out
+          );
+die "No entity number specified." unless $entity;
+while(<>) {
+  ($beg, $siz) = split;
+  print "INSERT INTO occurrence VALUES($entity, $beg, $siz);\n";
+}
